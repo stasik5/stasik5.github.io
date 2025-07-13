2ff62509d0cf612519cce7ceeab154990d73970a
stasik5 <116295795+stasik5@users.noreply.github.com>
2025-07-13 22:25:59 +0300

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,44 @@
+const listItems = document.querySelectorAll("li");
+
+function toggleDone(e) {
+  if (!e.target.className) {
+    e.target.className = "done";
+  } else {
+    e.target.className = "";
+  }
+}
+
+listItems.forEach((item) => {
+  item.addEventListener("click", toggleDone);
+});
+
+const myImage = document.querySelector("img");
+
+myImage.addEventListener("click", () => {
+  const mySrc = myImage.getAttribute("src");
+  if (mySrc === "images/cat.jpeg") {
+    myImage.setAttribute("src", "images/cat2.jpeg");
+  } else {
+    myImage.setAttribute("src", "images/cat.jpeg");
+  }
+});
+
+let myButton = document.querySelector("button");
+let myHeading = document.querySelector("h1");
+
+function setUserName() {
+  const myName = prompt("Please enter your name.");
+  localStorage.setItem("name", myName);
+  myHeading.textContent = `Eat all things, ${myName}`;
+}
+
+if (!localStorage.getItem("name")) {
+  setUserName();
+} else {
+  const storedName = localStorage.getItem("name");
+  myHeading.textContent = `Mozilla is cool, ${storedName}`;
+}
+
+myButton.addEventListener("click", () => {
+  setUserName();
+});
